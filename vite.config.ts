332ffd92655a8b9b447f2bb59279b74191374c19
Rajesh Tudu <tudurajesh34@gmail.com>
2026-01-19 12:33:00 +0545

    location changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Portfolio/", // <-- IMPORTANT (repo name)
   server: {
     host: "::",
     port: 8080,
-    hmr: { overlay: false },
+    hmr: {
+      overlay: false,
+    },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
